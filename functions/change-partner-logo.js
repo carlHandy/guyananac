f05fe6de6412e4e06c1d0fba21f73edd2d660ed6
Carl Handy <carl@kalpa.dev>
2021-11-18 19:37:30 -0400

    update change partner api

--- functions/change-partner-logo.js
+++ functions/change-partner-logo.js
@@ -1,27 +1,45 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const axios = require('axios');
 const cors = require("cors")({
   origin: true,
 });
 
-// handles the generation of access token
-exports.get360Token = functions.https.onRequest(async (req, res) => {
-    // allow cors request
-    return cors(req, res, () => {
-            let user = 'jon.lenton@maxsold.com';
-            return axios.post('https://maxsold.maxsold.com/mapi/auctions/updatepartnerlogo', {username: user}, {
-                headers: {
-                    'x-api-key': 'aa7f20e2d7a6204d30d7eadd3cb6841d9d0f97bd'
-                }
+// Check if firebase is authorized
+async function validateToken(token) {
+    try {
+      let response = await admin.auth().verifyIdToken(token);
+      return response;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+
+exports.get360Token = functions.https.onRequest( (request, response) => {
+    // enable cors 
+    cors(request, response, () => {
+        return validateToken(request.headers.authorization)
+        .then(() => {
+            // request to auction method api
+            let url = 'https://maxsold.maxsold.com';
+            axios.post(`${url}/mapi/auctions/updatepartnerlogo`, 
+            { 
+                username: "jon.lenton@maxsold.com"
+            }, 
+            {headers:{ 'x-api-key': 'aa7f20e2d7a6204d30d7eadd3cb6841d9d0f97bd'}
+        })
+            .then(r => { 
+              console.log("AM response", r);
+              response.send(r.data);
             })
-            .then(response => {
-                return res.status(200).json({
-                    message: response.data
-                })
-            }).catch(err => {
-                return res.status(500).json({
-                    error: err
-                })
+            .catch( e => {
+              console.log( "AM error: ", e);
+              response.sendStatus(500);
             })
-        }) 
-    });
+        }).catch((err) => {
+            return response.status(401).send(err);
+        });
+    })
+  
+  })
