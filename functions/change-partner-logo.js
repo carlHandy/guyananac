fff3e8568206a23f668167e422d72fbbe1396e7c
Carl Handy <carl@kalpa.dev>
2021-11-19 08:41:18 -0400

    fix token function

--- functions/change-partner-logo.js
+++ functions/change-partner-logo.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const axios = require('axios');
+var e = require('request');
 const cors = require("cors")({
   origin: true,
 });
@@ -15,31 +15,23 @@ async function validateToken(token) {
     }
   }
 
-
 exports.get360Token = functions.https.onRequest( (request, response) => {
     // enable cors 
     cors(request, response, () => {
+           // request to auction method api   
         return validateToken(request.headers.authorization)
         .then(() => {
-            // request to auction method api
-            let url = 'https://maxsold.maxsold.com';
-            axios.post(`${url}/mapi/auctions/updatepartnerlogo`, 
-            { 
-                username: "jon.lenton@maxsold.com"
-            }, 
-            {headers:{ 'x-api-key': 'aa7f20e2d7a6204d30d7eadd3cb6841d9d0f97bd'}
+            e.post(
+                'https://maxsold.maxsold.com/mapi/auth/token',
+                { json: { username: 'jon.lenton@maxsold.com' },
+                headers:{ 'x-api-key': 'aa7f20e2d7a6204d30d7eadd3cb6841d9d0f97bd'} },
+                function (error, res, body) {
+                    if (!error && res.statusCode == 200) {
+                        response.status(200).send({body});
+                    }
+                }
+                );
+          })
         })
-            .then(r => { 
-              console.log("AM response", r);
-              response.send(r.data);
-            })
-            .catch( e => {
-              console.log( "AM error: ", e);
-              response.sendStatus(500);
-            })
-        }).catch((err) => {
-            return response.status(401).send(err);
-        });
     })
   
-  })
