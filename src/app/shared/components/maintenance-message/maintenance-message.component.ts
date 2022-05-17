0759abf09e128ff6c4db7cf7b082f96665ab7171
Carl Handy <carl@kalpa.dev>
2022-05-17 14:01:18 -0400

    create component

---
+++ src/app/shared/components/maintenance-message/maintenance-message.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-message',
+  templateUrl: './maintenance-message.component.html',
+})
+export class MaintenanceMessageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
